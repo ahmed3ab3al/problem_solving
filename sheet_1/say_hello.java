6cd26dfbd3da283faa91b244bbf5fec2813feea8
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-07-24 23:19:52 +0300

    say_update

--- sheet_1/say_hello.java
+++ sheet_1/say_hello.java
@@ -1,13 +1,14 @@
 package problem_solving.sheet_1;
+
 import java.util.Scanner;
 
 public class say_hello {
     public static void main(String[] args) {
         String name;
-        Scanner input = new Scanner(System.in);
-       name =input.nextLine();
-       System.out.println("Hello, "+name);
+        try (Scanner input = new Scanner(System.in)) {
+            name = input.nextLine();
+        }
+        System.out.println("Hello, " + name);
 
+    }
 }
-}
-
