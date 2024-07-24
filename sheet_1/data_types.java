3ac4d1b159a2cc43228aec0b34363d24e83c3408
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-07-25 00:12:37 +0300

    update types

--- sheet_1/data_types.java
+++ sheet_1/data_types.java
@@ -10,20 +10,18 @@ public class data_types {
         float num3;
         double num4;
         try (Scanner input = new Scanner(System.in)) {
-            num = input.nextInt();
-            num2 = input.nextLong();
-            ch = input.next();
-            num3 = input.nextFloat();
-            num4 = input.nextDouble();
+            num=input.nextInt();
+            num2=input.nextLong();
+            ch=input.next();
+            num3=input.nextFloat();
+            num4=input.nextDouble();
         }
         System.out.println(num);
         System.out.println(num2);
-
         System.out.println(ch);
-
         System.out.println(num3);
-
         System.out.println(num4);
+
 
     }
 }
